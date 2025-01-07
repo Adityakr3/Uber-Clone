a3dc0f36f41d5fb484fb5fb5101346b9cb535869
Aditya kumar <83528896+Adityakr3@users.noreply.github.com>
2025-01-08 02:29:28 +0530

    correction in caption schema

--- Backend/models/caption.model.js
+++ Backend/models/caption.model.js
@@ -1,88 +1,88 @@
 const mongoose = require("mongoose");
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
-const captionSchema = new mongoose.Schema({
-  fullName: {
-    firstName: {
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const captainSchema = new mongoose.Schema({
+  fullname: {
+    firstname: {
       type: String,
-      require: true,
-      minLenght: [3, "firstName must be at least 3 char"],
+      required: true,
     },
-    lastName: {
+    lastname: {
       type: String,
-      minLenght: [3, "firstName must be at least 3 char"],
+      minlength: [3, "Lastname must be at least 3 characters long"],
     },
   },
   email: {
-    type: string,
-    require: true,
+    type: String,
+    required: true,
     unique: true,
     lowercase: true,
-    match: [
-      /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/,
-      "enter a vaild email",
-    ],
+    match: [/^\S+@\S+\.\S+$/, "Please enter a valid email"],
   },
-  password:{
-    type:string,
-    require:true,
-    select:false
+  password: {
+    type: String,
+    required: true,
+    select: false,
   },
-  socketId:{
-    type:String
- },
+  socketId: {
+    type: String,
+  },
 
- status:{
-    type:string,
-    enum:['active','inactive'],
-    default:'inactive'
- },
- vehicle:{
-    color:{
-        type:String,
-        require:true,
-        minLenght:[3,'color must be at least 3 characters long']
-    },
-    plate:{
-        type:String,
-        require:true,
-        minLenght:[3,'plate must be at least 3 characters long'] 
-    },
-    capacity:{
-        type: Number,
-        require:true,
-        minLenght:[1,'capacity must be at least 1'] 
-    },
-    vehicleType:{
-        type:String,
-        require:true,
-        enum:['car','motorcycle','auto']
-    }
- },
- location:{
-    lat:{
-        type:Number
-    },
-    lng:{
-        type:Number
-    }
- }
+  status: {
+    type: String,
+    enum: ["active", "inactive"],
+    default: "inactive",
+  },
 
+  vehicle: {
+    color: {
+      type: String,
+      required: true,
+      minlength: [3, "Color must be at least 3 characters long"],
+    },
+    plate: {
+      type: String,
+      required: true,
+      minlength: [3, "Plate must be at least 3 characters long"],
+    },
+    capacity: {
+      type: Number,
+      required: true,
+      min: [1, "Capacity must be at least 1"],
+    },
+    vehicleType: {
+      type: String,
+      required: true,
+      enum: ["car", "motorcycle", "auto"],
+    },
+  },
+
+  location: {
+    ltd: {
+      type: Number,
+    },
+    lng: {
+      type: Number,
+    },
+  },
 });
 
-captionSchema.methods.generateAuthToken = function(){
-        const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET_KEY , {expiresIn:'24h'})
-        return token;
-}
+captainSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET_KEY , {
+    expiresIn: "24h",
+  });
+  return token;
+};
 
-captionSchema.methods.comparePassword = async function(password){
-    return await bcrypt.compare(password , this.password)
-}
+captainSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
-captionSchema.statics.hashPassword = async function(password){
-    return await bcrypt.hash(password, 10)
-}
+captainSchema.statics.hashPassword = async function (password) {
+  return await bcrypt.hash(password, 10);
+};
 
-const captionModel = mongoose.model('caption',captionSchema)
+const captainModel = mongoose.model("captain", captainSchema);
 
-module.export = captionModel
+module.exports = captainModel;
