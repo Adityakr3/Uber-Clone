48207abb670503a94af518a8727c9b576f30674a
Aditya kumar <83528896+Adityakr3@users.noreply.github.com>
2025-01-09 00:25:31 +0530

    implement login, profile retrieval, and logout functionality for captains

--- Backend/controllers/caption.controller.js
+++ Backend/controllers/caption.controller.js
@@ -1,6 +1,7 @@
 const captainModel = require('../models/caption.model');
 const captainService = require('../services/caption.service');
 const {validationResult} = require('express-validator');
+const blacklistTokenModel = require('../models/blacklistToken.model');
 module.exports.registerCaptain = async (req, res, next) => {
 
     const errors = validationResult(req);
@@ -34,4 +35,44 @@ module.exports.registerCaptain = async (req, res, next) => {
 
     res.status(201).json({ token, captain });
 
+}
+
+module.exports.loginCaptain = async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    const captain = await captainModel.findOne({ email }).select('+password');
+
+    if (!captain) {
+        return res.status(401).json({ message: 'Invalid Email or Password' });
+    }
+
+    const isMatch = await captain.comparePassword(password);
+
+    if (!isMatch) {
+        return res.status(401).json({ message: 'Invalid Email or Password' });
+    }
+
+    const token = captain.generateAuthToken();
+
+    res.cookie('token', token);
+
+    res.status(200).json({ token, captain });
+
+}
+
+
+module.exports.getCaptainProfile = async (req, res, next) => {
+    res.status(200).json({ captain: req.captain });
+}
+
+module.exports.logoutCaptain = async (req, res, next) => {3
+    const token = req.cookies.token || req.headers.authorization.split(" ")[1];
+    await blacklistTokenModel.create({ token });
+    res.clearCookie('token');
+    res.status(200).json({ message: 'Captain logged out successfully' });
 }
