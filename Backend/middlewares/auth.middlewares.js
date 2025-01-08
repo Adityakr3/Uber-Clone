7ef1a2070703d8c755aba9e952fb67f00e6774d6
Aditya kumar <83528896+Adityakr3@users.noreply.github.com>
2025-01-09 00:26:32 +0530

    add authentication middleware for captains

--- Backend/middlewares/auth.middlewares.js
+++ Backend/middlewares/auth.middlewares.js
@@ -1,6 +1,8 @@
 const userModel = require("../models/user.model");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
+const blacklistTokenModel = require("../models/blacklistToken.model");
+const captainModel = require("../models/caption.model");
 
 module.exports.authUser = async (req, res, next) => {
   const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
@@ -22,5 +24,24 @@ module.exports.authUser = async (req, res, next) => {
   }
 };
 
-
-
+module.exports.authCaption = async (req, res, next) => {
+  const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  const isBlacklisted = await blacklistTokenModel.findOne({token:token});
+  console.log(isBlacklisted);
+  if (isBlacklisted) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    const user = await captainModel.findById(decoded._id)
+    req.captain = user;
+    return next();
+  }
+  catch (error) {
+    console.log(error);
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+}
