e2d16bcf4523a6326b9bf802b4c28f7be6e16724
Aditya kumar <83528896+Adityakr3@users.noreply.github.com>
2025-01-18 00:26:05 +0530

    recaptcha api add

--- Backend/app.js
+++ Backend/app.js
@@ -1,27 +1,33 @@
-const dotenv = require('dotenv');
-dotenv.config();
-const cookieParser = require('cookie-parser')
 const express = require('express');
-const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const connectToDb = require('./db/db');
 const userRoutes = require('./routes/user.routes');
 const captionRoutes = require('./routes/caption.routes');
-connectToDb()
+const recaptchaRoutes = require('./routes/recaptcha.routes');
+
+connectToDb();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+// Middleware to log request body
+app.use((req, res, next) => {
+    console.log('Request Body:', req.body);
+    next();
+});
 
-app.get('/users',(req , res)=>{
+app.get('/users', (req, res) => {
     res.send('Hello World');
 });
 
-app.use('/users', userRoutes)
-app.use('/captions', captionRoutes)
-
-
+app.use('/users', userRoutes);
+app.use('/captions', captionRoutes);
+app.use('/api', recaptchaRoutes);
 
 module.exports = app;
