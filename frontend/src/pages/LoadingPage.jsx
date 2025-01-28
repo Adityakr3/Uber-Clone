6bd4b1d265acca91f301db0436fe52784a687704
Aditya kumar <83528896+Adityakr3@users.noreply.github.com>
2025-01-29 01:32:08 +0530

    refactor: simplify LoadingPage by removing captcha handling and update routing paths

--- frontend/src/pages/LoadingPage.jsx
+++ frontend/src/pages/LoadingPage.jsx
@@ -1,22 +1,9 @@
-import React, { useState } from 'react'
+import React from 'react'
 import '../css/LoadingPage.css'
 import { Link, } from 'react-router-dom'
-import Recaptcha from '../captcha/ReCAPTCHA';
-import { useNavigate } from 'react-router-dom';
-import { ToastContainer, toast } from 'react-toastify';
+
 
 const LoadingPage = () => {
-    const [captchaToken, setCaptchaToken] = useState(null);
-    const navigate = useNavigate();
-
-    const handleCaptchatokenCheck = () => {
-        if (captchaToken) {
-            navigate('/login');
-        }else {
-            toast("Please verify the captcha",{type:'error'});
-        }
-    }
-
     return (
         <div className='bg-cover bg-[url(https://images.pexels.com/photos/29333180/pexels-photo-29333180/free-photo-of-street-view-in-savannah-with-fire-truck-responding.jpeg)] loading w-screen h-screen flex justify-center items-center'>
             <div className="cover w-screen h-screen bg-black bg-opacity-50 flex flex-col justify-center items-center">
@@ -27,18 +14,12 @@ const LoadingPage = () => {
                 <div class="center-box">
                     <div class="animated-border-box-glow"></div>
                     <div class="animated-border-box">
-                        <Link onClick={handleCaptchatokenCheck} className="bg-[black]  text-white px-48 py-7 rounded-lg text-4xl">
+                        <Link to="/usersignUp" className="bg-[black]  text-white px-48 py-7 rounded-lg text-4xl">
                             Continue
                         </Link>
                     </div>
                 </div>
-                <div className="mt-16">
-                    <Recaptcha onVerify={(token) => {
-                        setCaptchaToken(token);
-                    }} />
-                </div>
             </div>
-            <ToastContainer />
         </div>
     )
 }
