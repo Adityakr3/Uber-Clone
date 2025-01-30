4afa50c7f0c08e80544598bcb5a4e505cd931053
Aditya kumar <83528896+Adityakr3@users.noreply.github.com>
2025-01-30 19:52:12 +0530

    refactor: streamline user registration and login routes by removing validation middleware

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -1,20 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const {body} = require('express-validator');
 const userController = require('../controllers/user.controller');
 const authMiddleware = require('../middlewares/auth.middlewares');
-router.post('/register',[
-    body('email').isEmail().withMessage('Invaild Mail'),
-    body('fullName.firstName').isLength({min:5}).withMessage('FirstName is too short'),
-    body('password').isLength({min:8}).withMessage('password is too short'),
-],userController.registerUser)
+router.post('/register',userController.registerUser)
 
 
-router.post('/login',[
-    body('email').isEmail().withMessage('Invaild Mail'),
-    body('password').isLength({min:8}).withMessage('password is too short'),
-],userController.loginUser)
-
+router.post('/login',userController.loginUser)
 router.get('/profile', authMiddleware.authUser,  userController.getUserProfile);
 router.get('/logout' , authMiddleware.authUser,userController.logoutUser)
 
